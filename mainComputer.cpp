bdeedf14718ec089fc41ef206046f2634b2c03c5
Jay Patel <jayypatel657@gmail.com>
2026-02-25 15:59:20 -0500

    Delete mainComputer.cpp

--- mainComputer.cpp
+++
@@ -1,7 +0,0 @@
-#include "C4Board.h" //get the C4Board class
-
-int main() {
-    C4Board c4;
-    c4.playComputer(); //instead of just play game with 2 humans we have 1 human and then the other is the computer
-    return 0;
-}
